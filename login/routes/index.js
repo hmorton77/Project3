71af2ef191f62e8a833bc429a288a3c1c6471d37
BradleyEKaczmarski <69479265+BradleyEKaczmarski@users.noreply.github.com>
2021-02-01 19:08:34 -0500

    login troubleshooting

---
+++ login/routes/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const router = express.Router();
+//login page
+router.get('/', (req, res) => {
+    res.render('welcome');
+})
+//register page
+router.get('/register', (req, res) => {
+    res.render('register');
+})
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+module.exports = router; 
